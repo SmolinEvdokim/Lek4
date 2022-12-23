4cd3f242fab52a1c8a822e3a03d5a7b985012fd0
SmolinEvdokim <smolinevdok@gmail.com>
2022-12-23 18:41:27 +0100

    x.c

--- recursionAlg/Program.cs
+++ recursionAlg/Program.cs
@@ -1,4 +1,4 @@
-﻿ //string[,] table = new string[2, 5];
+﻿// string[,] table = new string[2, 5];
 // table[0,0] // table[0,1]// table[0,2]// table[0,4]
 // table[1,0] // table[1,1]// table[1,2]// table[1,4]
 // table[1, 2] = "slowo";
@@ -21,12 +21,57 @@
 // Console.WriteLine();
 // }
 
-int[,] matrix = new int[3, 4];
-for (int i = 0; i < matrix.GetLength(0); i++)
+ 
+
+// void PrintArray(int[,] matr)
+// {
+
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//         Console.WriteLine($"{matr[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// int[,] matrix = new int[3, 4];
+// PrintArray(matrix);
+
+
+
+void PrintArray(int[,] matr)
 {
-    for (int j = 0; j < matrix.GetLength(1); j++)
+
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
-        Console.WriteLine($" {matrix[i, j]}  ");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.WriteLine($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
     }
-Console.WriteLine();
 }
+
+void FillArray(int[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+              
+        }
+        Console.WriteLine();
+    }
+     
+}
+
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
